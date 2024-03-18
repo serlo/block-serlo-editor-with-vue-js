dd718a470acb9b338a5e640f70f2d5636e2257ac
Mikey Stengel <mikey@serlo.org>
2024-03-18 08:55:33 +0100

    chore: Try out veaury

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
+import veauryVitePlugins from 'veaury/vite/index.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,15 +13,18 @@ export default defineConfig({
     'process.env': loadEnv('', process.cwd())
   },
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag === 'react-wrapper'
+    veauryVitePlugins({
+      type: 'vue',
+      vueOptions: {
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag: string) => tag === 'react-wrapper'
+          }
         }
-      }
-    }),
-    vueJsx(),
-    react()
+      },
+      vueJsxOptions: {},
+      reactOptions: {}
+    })
   ],
   resolve: {
     alias: {
